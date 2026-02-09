ac27e65eb48b7b75d7681f821f2333d2d7ae2c26
Dennis Erdmann <dennis@erdmann-freunde.de>
2026-02-09 20:12:27 +0100

    Füge 'Theme Toolobx' Abschnitt hinzu und aktualisiere Übersetzungen; füge Icon für die Benutzeroberfläche hinzu

--- contao/config/config.php
+++ contao/config/config.php
@@ -12,9 +12,18 @@ declare(strict_types=1);
 
 use ErdmannFreunde\ThemeToolboxBundle\Backend\Maintenance\BypassScriptCache;
 
-$GLOBALS['BE_MOD']['design']['toolboxEditor'] = ['tables' => ['tl_toolbox_editor', 'tl_toolbox_editor_css']];
+// Create 'themeToolbox' section right after 'design'
+$offset = array_search('design', array_keys($GLOBALS['BE_MOD']));
+if (false !== $offset) {
+    $GLOBALS['BE_MOD'] = array_slice($GLOBALS['BE_MOD'], 0, $offset + 1, true)
+        + ['themeToolbox' => []]
+        + array_slice($GLOBALS['BE_MOD'], $offset + 1, null, true);
+} else {
+    $GLOBALS['BE_MOD']['themeToolbox'] = [];
+}
 
-$GLOBALS['BE_MOD']['design']['themeScssEditor'] = [];
+$GLOBALS['BE_MOD']['themeToolbox']['toolboxEditor'] = ['tables' => ['tl_toolbox_editor', 'tl_toolbox_editor_css']];
+$GLOBALS['BE_MOD']['themeToolbox']['themeScssEditor'] = [];
 
 array_unshift(
     $GLOBALS['TL_MAINTENANCE'],
